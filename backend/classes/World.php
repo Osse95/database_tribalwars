6150383a7e82e88bba9484962527f78774503787
Osse95 <81030754+Osse95@users.noreply.github.com>
2022-06-12 20:44:28 +0200

    add world class

---
+++ backend/classes/World.php
@@ -0,0 +1,49 @@
+<?php
+require_once "DB.php";
+
+class World extends DB
+{
+    private SimpleXMLElement $worldConfig;
+    private SimpleXMLElement $troopConfig;
+    private SimpleXMLElement $buildingConfig;
+
+
+    /**
+     * @throws Exception
+     */
+    function __construct($world = "Allgemein")
+    {
+        parent::__construct($world);
+        $worldConfig = $this->query("SELECT * FROM `config`");
+        $this->worldConfig = new SimpleXMLElement($worldConfig[0]["worldconfig"]);
+        $this->troopConfig = new SimpleXMLElement($worldConfig[0]["troupconfig"]);
+        $this->buildingConfig = new SimpleXMLElement($worldConfig[0]["buildingconfig"]);
+    }
+
+    function isArcherAvailable(): bool
+    {
+        if ($this->worldConfig->game->archer == "1") {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function isKnightAvailable(): bool
+    {
+        if ($this->worldConfig->game->knight > "0") {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    function isMilitiaAvailable(): bool
+    {
+        if ($this->worldConfig->game->militia > "0") {
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
