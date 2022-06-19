9022efd557d79a147a8e381756b6da0b5098d031
Osse95 <81030754+Osse95@users.noreply.github.com>
2022-06-20 01:11:18 +0200

    add sort functions

--- backend/classes/DataTables.php
+++ backend/classes/DataTables.php
@@ -30,6 +30,40 @@ class DataTables{
         };
     }
 
+    public static function sortAllAttacks($column): string
+    {
+        return match ($column) {
+            "1" => "defenderid",
+            "2" => "defendercoords",
+            "3" => "attackerid",
+            "4" => "attackercoords",
+            "5" => "reason",
+            "6" => "eingelesen_am",
+            "7" => "counter",
+            "8" => "predictedLabel",
+            "9" => "timeunix",
+            default => "type2",
+        };
+    }
+
+    public static function sortReportTable($column): string
+    {
+        return match ($column) {
+            "1" => "defender_nick",
+            "3" => "fighttime",
+            default => "attacker_nick"
+        };
+    }
+
+    public static function sortSupportReportTable($column): string
+    {
+        return match ($column) {
+            "1" => "defender_nick",
+            "3" => "support_time",
+            default => "supporter_nick"
+        };
+    }
+
     public static function sortBy($sort): string
     {
         if($sort == "asc"){
