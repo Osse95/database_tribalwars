f64800c17b0969661b85fe29e0ce2571b6a77eb5
Osse95 <81030754+Osse95@users.noreply.github.com>
2022-06-20 01:15:07 +0200

    remove show errors

--- ajax/attacks/getAllAttackTable.php
+++ ajax/attacks/getAllAttackTable.php
@@ -2,8 +2,6 @@
 session_start();
 
 if (!isset($_SESSION["name"]) or !isset($_POST)) return;
-error_reporting(E_ALL);
-ini_set('display_errors', 'On');
 require dirname(__DIR__, 2) . "/backend/classes/DB.php";
 require dirname(__DIR__, 2) . "/backend/classes/World_User.php";
 require dirname(__DIR__, 2) . "/backend/classes/Players.php";
