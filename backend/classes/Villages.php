877ef75414e330388ec3453274f2512e1ebd90fa
Osse95 <81030754+Osse95@users.noreply.github.com>
2022-06-19 17:41:52 +0200

    add new function to Reports

---
+++ backend/classes/Villages.php
@@ -0,0 +1,47 @@
+<?php
+
+require_once "DB.php";
+
+class Villages extends DB
+{
+    function __construct($world)
+    {
+        parent::__construct($world);
+    }
+
+    function getAllVillagesSortByID(): array
+    {
+        $return = [];
+        $query = $this->query("SELECT * FROM `dorfdaten`");
+        foreach($query as $village){
+            $return[$village["dorfid"]] = array("villageID"=>$village["dorfid"],
+                "villageName"=>$village["dorfname"],
+                "coords"=>$village["dorfcoords"],
+                "x"=>$village["dorfx"],
+                "y"=>$village["dorfy"],
+                "playerID"=>$village["spielerid"],
+                "tribeID"=>$village["tribe"],
+                "points"=>$village["dorfpunkte"],
+                "bonusID"=>$village["bonusid"]);
+        }
+        return $return;
+    }
+
+    function getAllVillagesSortByCoords(): array
+    {
+        $return = [];
+        $query = $this->query("SELECT * FROM `dorfdaten`");
+        foreach($query as $village){
+            $return[$village["dorfcoords"]] = array("villageID"=>$village["dorfid"],
+                "villageName"=>$village["dorfname"],
+                "coords"=>$village["dorfcoords"],
+                "x"=>$village["dorfx"],
+                "y"=>$village["dorfy"],
+                "playerID"=>$village["spielerid"],
+                "tribeID"=>$village["tribe"],
+                "points"=>$village["dorfpunkte"],
+                "bonusID"=>$village["bonusid"]);
+        }
+        return $return;
+    }
+}
