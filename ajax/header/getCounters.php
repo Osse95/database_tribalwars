205ed6001d7184acbcbcf2731cd510d4ed987ba1
Osse95 <81030754+Osse95@users.noreply.github.com>
2022-06-26 08:52:45 +0200

    fix require bug

--- ajax/header/getCounters.php
+++ ajax/header/getCounters.php
@@ -1,12 +1,13 @@
 <?php
 session_start();
 if(!isset($_SESSION["name"])) return;
-
-require dirname(__DIR__, 2) . "/backend/classes/World_User.php";
-require dirname(__DIR__, 2) . "/backend/classes/General.php";
-require dirname(__DIR__, 2) . "/backend/classes/Database/Attacks.php";
-require dirname(__DIR__, 2) . "/backend/classes/Database/Reports.php";
-require dirname(__DIR__, 2) . "/backend/classes/Database/SupportReports.php";
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+require_once dirname(__DIR__, 2) . "/backend/classes/World_User.php";
+require_once dirname(__DIR__, 2) . "/backend/classes/General.php";
+require_once dirname(__DIR__, 2) . "/backend/classes/Database/Attacks.php";
+require_once dirname(__DIR__, 2) . "/backend/classes/Database/Reports.php";
+require_once dirname(__DIR__, 2) . "/backend/classes/Database/SupportReports.php";
 
 $World_User = new World_User($_SESSION["name"],$_SESSION["world"]);
 if(!$World_User->isActivated()){
