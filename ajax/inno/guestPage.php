e5822097a522b0bc473761f95c004f502f31592a
Osse95 <81030754+Osse95@users.noreply.github.com>
2022-07-08 08:41:08 +0200

    optimize

--- ajax/inno/guestPage.php
+++ ajax/inno/guestPage.php
@@ -1,8 +1,14 @@
 <?php
 session_start();
 if(!isset($_SESSION["name"])) return;
+if(!isset($_POST["id"])) return;
 
 require_once dirname(__DIR__,2)."/backend/classes/Inno.php";
+require_once dirname(__DIR__,2)."/backend/classes/Player.php";
 
-$serverURL = Inno::getServerUrl($_SESSION["world"]);
-echo file_get_contents("$serverURL/guest.php?screen=info_player&id={$_POST["id"]}");
+$Player = new Player($_SESSION["world"],$_POST["id"]);
+
+if($Player->exists){
+    $serverURL = Inno::getServerUrl($_SESSION["world"]);
+    echo file_get_contents("$serverURL/guest.php?screen=info_player&id={$Player->getPlayerID()}");
+}
--- ajax/inno/guestTribePage.php
+++ ajax/inno/guestTribePage.php
@@ -3,6 +3,10 @@ session_start();
 if(!isset($_SESSION["name"])) return;
 
 require_once dirname(__DIR__,2)."/backend/classes/Inno.php";
+require_once dirname(__DIR__,2)."/backend/classes/Tribe.php";
 
-$serverURL = Inno::getServerUrl($_SESSION["world"]);
-echo file_get_contents("$serverURL/guest.php?screen=info_ally&id={$_POST["id"]}");
+$Tribe = new Tribe($_SESSION["world"],$_POST["id"]);
+if($Tribe->exists){
+    $serverURL = Inno::getServerUrl($_SESSION["world"]);
+    echo file_get_contents("$serverURL/guest.php?screen=info_ally&id={$Tribe->getTribeID()}");
+}
