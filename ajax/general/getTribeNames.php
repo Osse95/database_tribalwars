2c51e316b787a28e450b93fc286ab7cf6ad009fa
Osse95 <81030754+Osse95@users.noreply.github.com>
2022-07-10 12:35:47 +0200

    add ajax function for loading tribenames

---
+++ ajax/general/getTribeNames.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+if(isset($_SESSION["world"])){
+    $_POST["world"] = $_SESSION["world"];
+}
+if(!isset($_POST["world"])){
+    return;
+}
+require_once dirname(__DIR__,2)."/backend/classes/Tribes.php";
+require_once dirname(__DIR__,2)."/backend/classes/Inno.php";
+
+if(!Inno::existWorld($_POST["world"])){
+    $Return["result"] = false;
+    $Return["msg"] = "Bitte eine aktive Welt auswählen.";
+
+}else{
+    $Return["result"] = true;
+    $tribeNames = new Tribes($_POST["world"]);
+    $tribeNames = $tribeNames->getTribeNames();
+    $Return["tribeNames"] = $tribeNames;
+}
+echo json_encode($Return,true);
