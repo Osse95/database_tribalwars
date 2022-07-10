7e30ff8e338d59308072a13bd40040f8eaf5adc8
Osse95 <81030754+Osse95@users.noreply.github.com>
2022-07-10 12:35:24 +0200

    add different input types

--- assets/js/loadParams.js
+++ assets/js/loadParams.js
@@ -1,11 +1,19 @@
-function loadParams(){
+function loadParams() {
+    let selectOptions = ["coordType"];
+    let checkboxOptions = ["watchtower", "church", "academy"]
     let getParams = window.location.search.substr(1);
     getParams = getParams.split("&");
     for (let i = 0; i < getParams.length; i++) {
         let param = getParams[i].split("=");
-        if(param.length == 2){
-            $(`input[name=${param[0]}`).val(decodeURI(param[1]));
-            $(`input[name=${param[0]}`).trigger("change")
+        if (param.length == 2) {
+            if (selectOptions.includes(param[0])) {
+                $(`select[name=${param[0]}`).val(decodeURI(param[1]));
+            } else if (checkboxOptions.includes(param[0])) {
+                $(`input[name=${param[0]}`).prop('checked', true);
+            } else {
+                $(`input[name=${param[0]}`).val(decodeURI(param[1]));
+            }
         }
     }
+    $(`input`).trigger("change")
 }
