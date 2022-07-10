668a57a88f94003f0619b26af2f2eb274e320a0f
Osse95 <81030754+Osse95@users.noreply.github.com>
2022-07-10 17:35:12 +0200

    preparce backUp function for users

--- backend/classes/DB.php
+++ backend/classes/DB.php
@@ -1,20 +1,21 @@
 <?php
 
 require_once dirname(__DIR__, 2) . "/backend/config.php";
+require_once dirname(__DIR__, 2) . "/backend/classes/Mysqldump.php";
 
 class DB
 {
     public mysqli $conn;
     protected string $world;
 
-    function __construct($world="Allgemein")
+    function __construct($world = "Allgemein")
     {
         global $DB_Config;
         $this->conn = new mysqli($DB_Config["Server"], $world, $DB_Config["Password"], $world);
         $this->world = $world;
     }
 
-    function query($query)
+    function query($query): bool|array
     {
         $connection = $this->conn;
         $connection = $connection->query($query);
@@ -30,8 +31,28 @@ class DB
             return $rows;
         }
     }
-    function connectTo($DB){
+
+    function connectTo($DB)
+    {
         global $DB_Config;
         $this->conn = new mysqli($DB_Config["Server"], $DB, $DB_Config["Password"], $DB);
+    }
+
+    function createBackUp($world): bool
+    {
+        global $DB_Config;
+        $dbHost = $DB_Config["Server"];
+        $dbName = $this->world;
+        $dbUser = $this->world;
+        $dbPassword = $DB_Config["Password"];
+        $dumpFile = dirname(__DIR__, 2) . "/userBackups/" . $dbName . date("Y-m-d_H-i-s") . '.sql.gz';
+        try {
+            $dump = new Ifsnop\Mysqldump\Mysqldump("mysql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+            $dump->start($dumpFile);
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+
     }
 }
