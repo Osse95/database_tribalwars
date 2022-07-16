009432e1323ab52a87743aa7494f324137326e47
Osse95 <81030754+Osse95@users.noreply.github.com>
2022-07-16 18:40:53 +0200

    bugfix

--- assets/js/loadParams.js
+++ assets/js/loadParams.js
@@ -1,19 +1,22 @@
-function loadParams() {
-    let selectOptions = ["coordType"];
-    let checkboxOptions = ["watchtower", "church", "academy","internal"]
-    let getParams = window.location.search.substr(1);
-    getParams = getParams.split("&");
-    for (let i = 0; i < getParams.length; i++) {
-        let param = getParams[i].split("=");
-        if (param.length == 2) {
-            if (selectOptions.includes(param[0])) {
-                $(`select[name=${param[0]}`).val(decodeURI(param[1]));
-            } else if (checkboxOptions.includes(param[0])) {
-                $(`input[name=${param[0]}`).prop('checked', true);
-            } else {
-                $(`input[name=${param[0]}`).val(decodeURI(param[1]));
+ function loadParams() {
+            let selectOptions = ["coordType"];
+            let checkboxOptions = ["watchtower", "church", "academy", "internal"]
+            let getParams = window.location.search.substr(1);
+            getParams = getParams.split("&");
+            for (let i = 0; i < getParams.length; i++) {
+                let param = getParams[i].split("=");
+                if (param.length == 2) {
+                    if (selectOptions.includes(param[0])) {
+                        $(`select[name=${param[0]}`).val(decodeURI(param[1]));
+                        $(`select[name=${param[0]}`).trigger("change");
+                    } else if (checkboxOptions.includes(param[0])) {
+                        $(`input[name=${param[0]}`).prop('checked', true);
+                        $(`input[name=${param[0]}`).trigger("change");
+                    } else {
+                        $(`input[name=${param[0]}`).val(decodeURI(param[1]));
+                        $(`input[name=${param[0]}`).trigger("change");
+                    }
+                }
             }
-        }
     }
-    $(`input`).trigger("change")
-}
+
