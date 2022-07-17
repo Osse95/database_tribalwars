e19ba28b8881f516214550f687450a30fcd50993
Osse95 <81030754+Osse95@users.noreply.github.com>
2022-07-17 19:48:08 +0200

    add ajax functions for admin page

---
+++ ajax/admin/getAllVersions.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["name"])) return;
+require dirname(__DIR__, 2) . "/backend/classes/DB.php";
+require dirname(__DIR__, 2) . "/backend/classes/World_User.php";
+require dirname(__DIR__, 2) . "/backend/classes/Database/Admin.php";
+
+$World_User = new World_User($_SESSION["name"], $_SESSION["world"]);
+
+if (!$World_User->isActivated() || !$World_User->isAdmin()) {
+    General::redirectHeader();
+}
+
+$Admin = new Admin($World_User->getWorld(),$World_User->getVersion());
+$Return = $Admin->getAllVersions();
+
+echo json_encode($Return);
