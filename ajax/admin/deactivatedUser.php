7ce7bf4a4463908f4fa5db27b408a1695cd6d1c6
Osse95 <81030754+Osse95@users.noreply.github.com>
2022-07-19 16:37:00 +0200

    bugfix admin ajaxs

--- ajax/admin/deactivatedUser.php
+++ ajax/admin/deactivatedUser.php
@@ -4,7 +4,7 @@ session_start();
 if (!isset($_SESSION["name"])) return;
 require dirname(__DIR__, 2) . "/backend/classes/World_User.php";
 require dirname(__DIR__, 2) . "/backend/classes/Database/Admin.php";
-
+require dirname(__DIR__, 2) . "/backend/classes/General.php";
 $World_User = new World_User($_SESSION["name"], $_SESSION["world"]);
 
 if (!$World_User->isAdmin()) {
--- ajax/admin/getAllVersions.php
+++ ajax/admin/getAllVersions.php
@@ -5,7 +5,7 @@ if (!isset($_SESSION["name"])) return;
 require dirname(__DIR__, 2) . "/backend/classes/DB.php";
 require dirname(__DIR__, 2) . "/backend/classes/World_User.php";
 require dirname(__DIR__, 2) . "/backend/classes/Database/Admin.php";
-
+require dirname(__DIR__, 2) . "/backend/classes/General.php";
 $World_User = new World_User($_SESSION["name"], $_SESSION["world"]);
 
 if (!$World_User->isActivated() || !$World_User->isAdmin()) {
